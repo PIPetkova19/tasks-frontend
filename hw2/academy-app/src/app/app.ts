40786ac691b9ae7e0385ca3ca696e2c455eada45
PIPetkova19 <PIPetkova19@codingburgas.bg>
2026-05-24 23:11:43 +0300

    Render student list with ngFor and live search

--- hw2/academy-app/src/app/app.ts
+++ hw2/academy-app/src/app/app.ts
@@ -1,17 +1,49 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StudentCard } from './components/student-card/student-card';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StudentCard],
+  imports: [RouterOutlet, StudentCard, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App {
   protected readonly title = signal('academy-app');
-  name: string = 'Petya';
-  role: string = 'Intern';
-  skills: string[] = ['Html', 'Css', 'Js'];
+  students = [
+    {
+      name: 'Petya',
+      role: 'Intern',
+      skills: ['Html', 'Css', 'Js']
+    },
+    {
+      name: 'Rado',
+      role: 'Intern',
+      skills: ['Html', 'Css', 'Js']
+    },
+    {
+      name: 'Mimi',
+      role: 'Intern',
+      skills: ['Html', 'Css', 'Js']
+    },
+    {
+      name: 'Petya',
+      role: 'Intern',
+      skills: ['Html', 'Css', 'Js']
+    },
+    {
+      name: 'Petya',
+      role: 'Intern',
+      skills: ['Html', 'Css', 'Js']
+    }
+  ];
+
+  searchTerm: string = '';
+
+  filterStudents(searchTerm: string) {
+    return this.students.filter((student) => student.name.toLowerCase() === searchTerm.toLowerCase());
+  }
 }
