0d6ec2bbe1f627112099b7f3d5e24480da51c472
PIPetkova19 <PIPetkova19@codingburgas.bg>
2026-06-09 00:44:21 +0300

    Add style to task 11

--- hw3/timer-app/src/app/components/parent/parent.ts
+++ hw3/timer-app/src/app/components/parent/parent.ts
@@ -1,11 +1,16 @@
-import { Component,signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TimerComponent } from '../timer/timer';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-parent',
-  imports: [FormsModule,TimerComponent],
   templateUrl: './parent.html',
   styleUrl: './parent.css',
+  imports: [FormsModule, TimerComponent, MatButtonModule,
+    MatFormFieldModule, MatInputModule
+  ],
 })
 
 export class ParentComponent {
