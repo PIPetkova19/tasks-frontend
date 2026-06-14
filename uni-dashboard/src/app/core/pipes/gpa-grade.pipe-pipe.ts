cdcf3d0e1b611f2fe71cb8e381a2fe44a47251dc
PIPetkova19 <PIPetkova19@codingburgas.bg>
2026-06-15 00:41:57 +0300

    Add custom gpa pipe

--- uni-dashboard/src/app/core/pipes/gpa-grade.pipe-pipe.ts
+++ uni-dashboard/src/app/core/pipes/gpa-grade.pipe-pipe.ts
@@ -4,7 +4,16 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'gpaGradePipe',
 })
 export class GpaGradePipePipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number): string {
+    if (value >= 3.7) { return 'A'; }
+    else if (value >= 3.3) { return 'A-'; }
+    else if (value >= 3.0) { return 'B+'; }
+    else if (value >= 2.7) { return 'B'; }
+    else if (value >= 2.3) { return 'B-'; }
+    else if (value >= 2.0) { return 'C+'; }
+    else if (value >= 1.7) { return 'C'; }
+    if (value >= 1.7) return 'C';
+
+    return 'F';
   }
 }
